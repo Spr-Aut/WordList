4042c86c4e94268607ebb59813e7346f1702196b
Spr-Aut <ff1493706520@163.com>
2023-02-01 21:08:03 +0800

    NavigationBase

---
+++ app/src/main/java/com/example/wordlist/activity/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.wordlist.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import com.example.wordlist.R;
+import com.example.wordlist.adapter.TabPagerAdapter;
+
+public class MainActivity extends AppCompatActivity {
+    private ViewPager viewPager;
+    private RadioGroup radioGroup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        radioGroup=findViewById(R.id.rg_tabbar);
+        radioGroup.setOnCheckedChangeListener((group, checkedId) -> {
+            for(int pos=0;pos<radioGroup.getChildCount();pos++){
+                RadioButton radioButton=(RadioButton) radioGroup.getChildAt(pos);
+                if (radioButton.getId()==checkedId){
+                    viewPager.setCurrentItem(pos);
+                }
+            }
+        });
+
+        //创建一个翻页适配器
+        TabPagerAdapter adapter=new TabPagerAdapter(getSupportFragmentManager());
+        viewPager=findViewById(R.id.vp_content);
+        viewPager.setAdapter(adapter);
+        viewPager.addOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener(){
+            @Override
+            public void onPageSelected(int position) {
+                radioGroup.check(radioGroup.getChildAt(position).getId());
+            }
+        });
+    }
+}
