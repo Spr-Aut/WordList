1081ec8b7761dd6c603e305ee2f603c0773ed290
Spr-Aut <ff1493706520@163.com>
2023-02-26 19:27:56 +0800

    将主页面更换为DrawerLayout，设置页面换到侧滑的抽屉中

---
+++ app/src/main/java/com/example/wordlist/fragment/StatisticsFragment.java
@@ -0,0 +1,63 @@
+package com.example.wordlist.fragment;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.wordlist.R;
+
+public class StatisticsFragment extends Fragment {
+    private static final String TAG = "StatisticsFragment";
+    protected View mView; // 声明一个视图对象
+    protected Context mContext; // 声明一个上下文对象
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        mContext = getActivity(); // 获取活动页面的上下文
+        // 根据布局文件fragment_tab_third.xml生成视图对象
+        mView = inflater.inflate(R.layout.fragment_main, container, false);
+
+
+
+
+        return mView;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG,"启动");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG,"继续");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG,"停止");
+    }
+
+
+
+    /**
+     * 切换Fragment可视状态
+     * */
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
+        if (isVisibleToUser){
+            Log.d(TAG,"切换可视状态");
+        }else {
+        }
+    }
+
+}
